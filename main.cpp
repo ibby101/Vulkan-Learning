22a13ccdb53a7cc95764197ee4bf2ce0f58fe27b
Ibrahim Abdi <ibroabdi@outlook.com>
2025-09-09 13:09:22 +0100

    Added instance creation to project

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ const uint32_t HEIGHT = 600;
 class HelloTriangleApplication {
 public:
 	void run() {
+		createInstance();
 		initWindow();
 		initVulkan();
 		mainLoop();
@@ -19,6 +20,7 @@ public:
 
 private:
 	GLFWwindow* window;
+	VkInstance instance;
 
 	void initWindow() {
 		glfwInit();
@@ -30,7 +32,40 @@ private:
 	}
 
 	void initVulkan() {
+		createInstance();
+	}
 
+	void createInstance() {
+
+		// ---------------------- Application Info ----------------------
+		VkApplicationInfo appInfo{};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Hello Triangle!";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+
+
+		// ---------------------- Instance Creation Info ----------------------
+		VkInstanceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+
+		// ---------------------- Extensions ----------------------
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+		createInfo.enabledExtensionCount = glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+		// ---------------------- Validation Layers ----------------------
+		// Ignored for now
+		createInfo.enabledLayerCount = 0;
+
+		// ---------------------- Create the Instance ----------------------
+		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create instance!");
+		}
 	}
 
 	void mainLoop() {
