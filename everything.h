d6a7e7c35b64b12e3396af50fa703cf2f97ac1b9
Ibrahim Abdi <ibroabdi@outlook.com>
2025-09-19 19:31:33 +0100

    moved include and definitions to header file

--- everything.h
+++ everything.h
@@ -1,1 +1,23 @@
 #pragma once
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#include <vector>
+#include <cstring>
+
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+const std::vector<const char*> validationLayers = {
+	"VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif
